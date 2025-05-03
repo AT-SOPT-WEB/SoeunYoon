095cd1182f8190f8aef404aefa82399d070dfd59
Dubabbi <thdms3160@naver.com>
2025-05-04 00:32:36 +0900

    feat: 헤더에 페이지 이동 로직 추가

--- week4-assignment/src/layouts/Header.tsx
+++ week4-assignment/src/layouts/Header.tsx
@@ -1,7 +1,13 @@
+import { useLocation, useNavigate } from 'react-router-dom';
 import Logo from '../assets/logo.png';
 import Profile from '../assets/profile.jpg';
 
 export default function Header() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const isActive = (path: string) => location.pathname === path;
+
   return (
     <header className="fixed top-0 left-0 z-50 w-full bg-white shadow-sm px-6 py-4 flex items-center justify-between">
       <div className="flex items-center gap-2">
@@ -9,17 +15,37 @@ export default function Header() {
         <span className="text-darkSky font-bold text-lg">회원 조회 서비스</span>
       </div>
 
-      <nav className="flex gap-10">
-        <span className="underline text-black text-sm cursor-pointer">내 정보</span>
-        <span className="underline text-darkSky text-sm font-semibold cursor-pointer">
+      <nav className="flex gap-10 text-sm">
+        <span
+          onClick={() => navigate('/mypage/Info')}
+          className={`underline cursor-pointer ${
+            isActive('/mypage/Info') ? 'text-darkSky font-semibold' : 'text-black hover:text-darkSky-hover active:text-darkSky-active'
+          }`}
+        >
+          내 정보
+        </span>
+        <span
+          onClick={() => navigate('/mypage/search')}
+          className={`underline cursor-pointer ${
+            isActive('/mypage/search') ? 'text-darkSky font-semibold' : 'text-black hover:text-darkSky-hover active:text-darkSky-active'
+          }`}
+        >
           SOPT 회원 조회
         </span>
-        <span className="underline text-black text-sm cursor-pointer">로그아웃</span>
+        <span
+          onClick={() => {
+            localStorage.removeItem('accessToken');
+            navigate('/login'); 
+          }}
+          className="underline cursor-pointer text-black hover:text-darkSky-hover active:text-darkSky-active"
+        >
+        로그아웃
+        </span>
       </nav>
 
       <div className="flex items-center gap-2 px-3 py-1 rounded-full bg-lightSky">
         <img src={Profile} alt="profile" className="w-6 h-6 rounded-full" />
-        <span className="text-sm font-medium">유저네임</span>
+        <span className="text-sm font-medium text-black">유저네임</span>
       </div>
     </header>
   );
