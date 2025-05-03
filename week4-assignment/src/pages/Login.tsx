6a6534843e30d91ff8aa2d2e70310dcb91e6f968
Dubabbi <thdms3160@naver.com>
2025-05-03 23:43:16 +0900

    feat: 회원가입 UI 구현

--- week4-assignment/src/pages/Login.tsx
+++ week4-assignment/src/pages/Login.tsx
@@ -1,8 +1,11 @@
+import { useNavigate } from 'react-router';
+
 import AuthLayout from '../components/login/AuthLayout';
 import Input from '../components/common/Input';
 import Button from '../components/common/Button';
 
 export default function Login() {
+  const navigate = useNavigate();
   return (
     <AuthLayout title="로그인" highlight="SUS">
       <div className="flex flex-col gap-6">
@@ -11,7 +14,7 @@ export default function Login() {
 
         <div className="flex flex-col gap-3 mt-4">
           <Button>로그인</Button>
-          <Button variant="secondary">회원가입</Button>
+          <Button variant="secondary" onClick={() => navigate('/signup')}>회원가입</Button>
         </div>
       </div>
     </AuthLayout>
--- week4-assignment/src/pages/Signup.tsx
+++ week4-assignment/src/pages/Signup.tsx
@@ -1,8 +1,21 @@
-export default function Signup() {
-    return (
-      <div>
+import { useNavigate } from 'react-router';
 
+import AuthLayout from '../components/login/AuthLayout';
+import Input from '../components/common/Input';
+import Button from '../components/common/Button';
+
+export default function Signup() {
+  const navigate = useNavigate();
+  return (
+    <AuthLayout title="회원가입" highlight="SUS" optionalText="SOPT User Search에 오신 것을 환영합니다!">
+      <div className="flex flex-col gap-6">
+        <Input label="아이디" placeholder="아이디를 입력해 주세요" />
+
+        <div className="flex flex-col gap-3 mt-4 mb-2">
+          <Button>다음</Button>
+          <Button variant="secondary" onClick={() => navigate('/login')}>로그인</Button>
+        </div>
       </div>
-    );
-  }
-  
+    </AuthLayout>
+  );
+}
