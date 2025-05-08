8b0020c29bc7ee766b892d90937d8d43c45b4807
Dubabbi <thdms3160@naver.com>
2025-05-08 16:25:53 +0900

    feat: 조건부 리다이렉트 처리

--- week4-assignment/src/route/mainRoutes.tsx
+++ week4-assignment/src/route/mainRoutes.tsx
@@ -1,12 +1,26 @@
-import { lazy } from 'react';
-
-import { RouteObject } from 'react-router-dom';
+import { lazy, useEffect } from 'react';
+import { useNavigate, RouteObject } from 'react-router-dom';
 
 const MyInfo = lazy(() => import('../pages/MyInfo'));
 const Search = lazy(() => import('../pages/Search'));
 const Layout = lazy(() => import('../layouts/Layout'));
 
+const RedirectRoot = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('userId');
+    navigate(token ? '/mypage/info' : '/login', { replace: true });
+  }, [navigate]);
+
+  return null;
+};
+
 const mainRoutes: RouteObject[] = [
+  {
+    path: '/',
+    element: <RedirectRoot />,
+  },
   {
     path: '/',
     element: <Layout />,
